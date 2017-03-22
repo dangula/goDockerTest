2f138101fef2ed18acdedd04f8e35b050448c810
Divesh Angula <divesh.angula@quantum.com>
2017-03-21 21:37:35 -0700

    splitting block integration test into seperate tests

--- integrationTests/blockTests/blockStorage_test.go
+++ integrationTests/blockTests/blockStorage_test.go
@@ -1,32 +1,41 @@
 package integrationTests
 
 import (
-	"testing"
 	helper "github.com/dangula/goDockerTest/helpers"
-	"github.com/stretchr/testify/assert"
 	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 const BLOCK_PATH = "/tmp/rook1"
 const BLOCK_DATA = "writing in folder provisioned by rook"
 
-func TestBlockStoage_integrationTest(t *testing.T) {
-	BLOCK_FILE_NAME := uuid.NewV4().String()
-	t.Log("Test Block Storage mount - provioned by rook")
-
-	t.Log("Make sure mount directory is present")
-	if _,err := helper.IsDirectoryPresent(BLOCK_PATH); err != nil{
+func TestIfBlockStorageMountIsPresent(t *testing.T) {
+	t.Log("Make sure block mount is present")
+	if _, err := helper.IsDirectoryPresent(BLOCK_PATH); err != nil {
 		t.Log(err)
 		t.Errorf("Expected Block mount to be presnet")
 		t.Fail()
 
 	}
+}
 
+func TestWritingToBlockStorageMount(t *testing.T) {
+	BLOCK_FILE_NAME := uuid.NewV4().String()
 	t.Log("Make sure you can write to block storage mount")
 	if _, err := helper.WriteToFile(BLOCK_PATH, BLOCK_FILE_NAME, BLOCK_DATA); err != nil {
 		t.Log(err)
 		t.Errorf("Cannot Write to Block Storage Mount")
 		t.Fail()
+	}
+
+}
+
+func TestReadingFromBlockStorageMount(t *testing.T) {
+	BLOCK_FILE_NAME := uuid.NewV4().String()
+	_, err := helper.WriteToFile(BLOCK_PATH, BLOCK_FILE_NAME, BLOCK_DATA)
+	if err != nil {
+		panic(err)
 	}
 	t.Log("Make sure you can read from block storage mount")
 	if readData, err := helper.ReadFile(BLOCK_PATH, BLOCK_FILE_NAME); err != nil {
@@ -34,7 +43,7 @@ func TestBlockStoage_integrationTest(t *testing.T) {
 		t.Errorf("Cannot Read From Block Strorage Mount")
 		t.Fail()
 	} else {
-		assert.Equal(t,readData,BLOCK_DATA,"content of the file should be unchanged")
+		assert.Equal(t, readData, BLOCK_DATA, "content of the file should be unchanged")
 
 	}
 
