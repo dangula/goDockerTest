266c6ed792e52f4070e71abf9fbe3fcc195abc48
Divesh Angula <divesh.angula@quantum.com>
2017-03-28 20:23:05 -0700

    test for s3 client

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"github.com/minio/minio-go"
 
+)
+
+
+// Lists all objects in a bucket using pagination
+//
+// Usage:
+// listObjects <bucket>
 func main() {
-	fmt.Println("Hello World!")
+	// Note: YOUR-ACCESSKEYID, YOUR-SECRETACCESSKEY and my-bucketname are
+	// dummy values, please replace them with original values.
 
+	// Requests are always secure (HTTPS) by default. Set secure=false to enable insecure (HTTP) access.
+	// This boolean value is the last argument for New().
+
+	// New returns an Amazon S3 compatible client object. API compatibility (v2 or v4) is automatically
+	// determined based on the Endpoint value.
+	s3Client, err := minio.New("10.101.76.217:53390", "G4ZU50JKHOBG3N8NUH88",
+		"gmNoXmRsJRJwwJ0KGCy4NCbbfOc7aR1sUSeUfVOu", true)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = s3Client.MakeBucket("my-bucketname", "us-east-1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Success")
 }
