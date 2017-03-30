0280d0373c887318375709f386d7b7f60dd1251d
Divesh Angula <divesh.angula@quantum.com>
2017-03-29 20:54:13 -0700

    final clean ups

--- TestPodsSetUp.sh
+++ TestPodsSetUp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-
+#Functction to check if pod is up
+#Takes atleast 2 paramaters
+#$1 - Pod name mask - required
+#$2 - number of pods expected to be up that match the pod name($1) - required
+#$3 - namespace the pods are expected to be in - optional
 isPodUp(){
   	x=1
 	while [ $x -le 15 ]
@@ -28,6 +32,11 @@ isPodUp(){
 
 }
 
+#Functction to check if pod is in Running status or not
+#Takes atleast 2 paramaters
+#$1 - Pod name mask - required
+#$2 - number of pods expected to be in Running state that match the pod name($1) - required
+#$3 - namespace the pods are expected to be in - optional
 isPodRunning(){
   	x=1
 	while [ $x -le 15 ]
@@ -55,10 +64,14 @@ isPodRunning(){
 }
 
 
+#Funciton to check results in a test pod
+#This function periodically checks the test pod logs to see if test passed
+#Exists successfully if tests have passed and logged a "PASS" message
+#Exits with return code 1 if tests have not passed.
 checkTestResult(){
 	pod=$1
 	x=1
-	while [ $x -le 8 ]
+	while [ $x -le 10 ]
 	do
 	  res=$(kubectl logs $pod|tail -2| head -1)
 	  if [ "$res" == "PASS" ]
@@ -70,6 +83,11 @@ checkTestResult(){
 	done
 	return 1
 }
+
+#Funciton to check results in a test pod in a namespace
+#This function periodically checks the test pod logs to see if test passed
+#Exists successfully if tests have passed and logged a "PASS" message
+#Exits with return code 1 if tests have not passed.
 
 checkTestResultInNameSpace(){
 	pod=$1
@@ -89,7 +107,10 @@ checkTestResultInNameSpace(){
 
 
 
-
+#Functction to check if Test pod is up and running
+#Takes atleast 1 paramaters
+#$1 - Pod name mask - required
+#$2 - namespace the pods are expected to be in - optional
 isTestPodUp(){
 	TestPodName=$1
 	if [ "$#" -eq 2 ]; then
@@ -134,11 +155,13 @@ case $1 in
 
     #Run Block Tests
     [Bb][Ll][Oo][Cc][Kk])
-                echo "Create rook-storageclass"
-                export MONS=$(kubectl -n rook get pod mon0 mon1 mon2 -o json|jq ".items[].status.podIP"|tr -d "\""|sed -e 's/$/:6790/'|paste -s -d, -)
-                sed 's#INSERT_HERE#'$MONS'#' rook/rook-storageclass.yaml | kubectl create -f -
-                sleep 10
-		echo "Starting Block Test Pod"
+        echo "Create rook-storageclass"
+        kubectl create -f rook/rook-pool.yaml
+        sleep 10
+        export MONS=$(kubectl -n rook get pod mon0 mon1 mon2 -o json|jq ".items[].status.podIP"|tr -d "\""|sed -e 's/$/:6790/'|paste -s -d, -)
+        sed 's#INSERT_HERE#'$MONS'#' rook/rook-storageclass.yaml | kubectl create -f -
+        sleep 5
+		echo "Starting Block Test Pod with block storage volume mounted"
 		sed 's#INSERT_TEST_TYPE#'block'#' rook/block_test.yaml | kubectl create -f -
                 testPod=$?
 		if [ $testPod -ne 0 ]; then
@@ -160,22 +183,22 @@ case $1 in
 			echo "Block End 2 End Test Failed"
 			kubectl delete -f rook/block_test.yaml
 		    kubectl delete -f rook/rook-storageclass.yaml
+		    kubectl delete -f rook/rook-pool.yaml
 			exit 1
 		fi
 		kubectl delete -f rook/block_test.yaml
 		kubectl delete -f rook/rook-storageclass.yaml
+		kubectl delete -f rook/rook-pool.yaml
 		exit 0
          ;;
     #Run Object Test
-    [Oo][Bb][Jj][Ee][Cc][Tt])
-		echo "start Object Test Pod"
+    [Oo][Bb][Jj][Ee][Cc][Tt])		echo "Start File Test Pod with Filesystem mounted"
+
+		echo "Create object store and a user - get connection info"
 		kubectl exec -it rook-client -n rook -- rook object create
-	        kubectl exec -it rook-client -n rook -- rook object user create rook-user "A rook rgw User"
-                eval $(kubectl exec -it rook-client -n rook -- rook object connection rook-user --format env-var)
-		echo $AWS_HOST
-                echo $AWS_ENDPOINT
-                echo $AWS_ACCESS_KEY_ID
-		echo $AWS_SECRET_ACCESS_KEY
+	    kubectl exec -it rook-client -n rook -- rook object user create rook-user "A rook rgw User"
+        eval $(kubectl exec -it rook-client -n rook -- rook object connection rook-user --format env-var)
+		echo "start Object Test Pod with rook object store connection information"
   		sed 's#INSERT_TEST_TYPE#'object'#;s#AWS_ENDPOINT_VALUE#'$AWS_ENDPOINT'#;s#AWS_KEY_VALUE#'$AWS_ACCESS_KEY_ID'#;s#AWS_SECRET_VALUE#'$AWS_SECRET_ACCESS_KEY'#' rook/object_test.yaml | kubectl create -f -
 		testPod=$?
 		if [ $testPod -ne 0 ]; then
@@ -203,11 +226,12 @@ case $1 in
          ;;
     #Run File Test
     [Ff][Ii][Ll][Ee])
-		echo "Start File Test Pod"
+		echo "set up filessytem in rook"
 		kubectl exec -it rook-client -n rook -- rook filesystem create --name testfs
 		export CEPH_MON0=$(kubectl -n rook get pod mon0 -o json|jq ".status.podIP"|tr -d "\""|sed -e 's/$/:6790/')
 		export CEPH_MON1=$(kubectl -n rook get pod mon1 -o json|jq ".status.podIP"|tr -d "\""|sed -e 's/$/:6790/')
 		export CEPH_MON2=$(kubectl -n rook get pod mon2 -o json|jq ".status.podIP"|tr -d "\""|sed -e 's/$/:6790/')
+		echo "Start File Test Pod with Filesystem storage volume mounted"
 		sed 's#INSERT_TEST_TYPE#'file'#;s#CEPH_MON0#'$CEPH_MON0'#;s#CEPH_MON1#'$CEPH_MON1'#;s#CEPH_MON2#'$CEPH_MON2'#' rook/file_test.yaml | kubectl create -f -
                 testPod=$?
 		if [ $testPod -ne 0 ]; then
